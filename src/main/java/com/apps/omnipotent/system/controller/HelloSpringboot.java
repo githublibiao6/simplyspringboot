955ec89774b1eb686537e072e63218c7bfe15e48
李标 <1446977718@qq.com>
2019-07-09 23:58:39 +0800

    springboot简单返回页面的例子(页面打算用html，vue，js，react等、、、加油哦)

--- src/main/java/com/apps/omnipotent/system/controller/HelloSpringboot.java
+++ src/main/java/com/apps/omnipotent/system/controller/HelloSpringboot.java
@@ -1,14 +1,15 @@
 package com.apps.omnipotent.system.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloSpringboot {
 
     @RequestMapping("/hello")
     public String say() {
         System.out.println("Hello springboot");
-        return "hello,this is a springboot demo";
+        return "/login";
     }
 }
