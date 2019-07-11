111df59694091b85d993b8b521eecdaa727e3d60
李标 <1446977718@qq.com>
2019-07-12 00:32:42 +0800

    vue 不能直接像js引入，用了前端项目学习vue，还行，devtools热部署不行，会在重启后有残留的java进程，没法运行。注释了

--- src/main/java/com/apps/omnipotent/system/controller/HelloSpringboot.java
+++ src/main/java/com/apps/omnipotent/system/controller/HelloSpringboot.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloSpringboot {
 
+    /**
+     * @Author cles
+     * @Description 
+     * @Date 23:14 2019/7/10
+     * @return java.lang.String
+     **/
     @RequestMapping("/hello")
     public String say() {
-        System.out.println("Hello springboot");
+        System.out.println("Hello spr1inqgboo1t");
         return "/login";
     }
 }
