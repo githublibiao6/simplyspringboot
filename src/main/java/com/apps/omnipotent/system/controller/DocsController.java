8d961fbfdc5c81c6b12641acdf1ef75b73c94a62
李标 <1446977718@qq.com>
2020-04-17 00:30:53 +0800

    系统文档

---
+++ src/main/java/com/apps/omnipotent/system/controller/DocsController.java
@@ -0,0 +1,42 @@
+package com.apps.omnipotent.system.controller;
+
+import com.apps.omnipotent.system.core.controller.GlobalController;
+import com.apps.omnipotent.system.core.model.Result;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+* @Description: 文档
+* @Author: cles
+* @Date: 2020/4/16 22:17
+*/
+@Controller
+@RequestMapping("/document")
+public class DocsController extends GlobalController {
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public Result index() {
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        // 请求路径
+        String requestURI = request.getRequestURI();
+        String url = "http://"+address.getHostAddress() +":"+request.getServerPort()+"/"+request.getContextPath()
+                +"/static/document/_book/index.html";
+        result.setData(url);
+        return result;
+    }
+
+    @RequestMapping("/indexUrl")
+    public String url() {
+        return "document/_book/index";
+    }
+}
