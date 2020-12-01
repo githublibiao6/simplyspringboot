ba7c79dcc2c73dd7893ac6c5b1da120dd863dfd6
cles <1446977718@qq.com>
2020-12-01 23:46:50 +0800

    mongo的一些基础操作

---
+++ src/main/java/com/apps/omnipotent/system/mongodb/service/MongoService.java
@@ -0,0 +1,97 @@
+package com.apps.omnipotent.system.mongodb.service;
+/*
+ *  Created by cles on 2020/12/1 22:44
+ */
+
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.util.List;
+
+/**
+ * @author cles
+ * @description:
+ * @Date 2020/12/1 22:44
+ * @version: 1.0.0
+ */
+public interface MongoService<T>{
+
+    /**
+     * 保存一个对象
+     * @param t
+     */
+    void save(T t);
+
+    /**
+     * 根据对象Id查找该对象
+     * @param cls
+     * @param id
+     * @return
+     */
+    T queryById(Class<T> cls,String id);
+
+    /**
+     * 根据条件查询集合
+     * @param cls
+     * @param query
+     * @return
+     */
+    List<T> queryList(Class<T> cls, Query query);
+
+    /**
+     * 通过条件查询单个实体
+     * @param cls
+     * @param query
+     * @return
+     */
+    T queryOne(Class<T> cls,Query query);
+
+    /**
+     * 分页查询
+     * @param cls
+     * @param query
+     * @param start
+     * @param size
+     * @return
+     */
+    List<T> getPage(Class<T> cls,Query query, int start, int size);
+
+    /**
+     * 查询符合条件的记录总数
+     * @param cls
+     * @param query
+     * @return
+     */
+    Long getPageCount(Class<T> cls,Query query);
+
+    /**
+     * 根据id删除对象
+     * @param cls
+     * @param id
+     */
+    void deleteById(Class<T> cls,String id);
+
+    /**
+     * 删除对象
+     * @param t
+     */
+    void delete(T t);
+
+    /**
+     * 更新指定id的属性值
+     * @param cls
+     * @param id
+     * @param objName
+     * @param objValue
+     */
+    void updateFirst(Class<T> cls,String id, String objName, String objValue);
+
+    /**
+     * 查找更新
+     * @param cls
+     * @param query
+     * @param update
+     */
+    void updateInser(Class<T> cls,Query query, Update update);
+
+}
---
+++ src/main/java/com/apps/omnipotent/system/mongodb/service/impl/MongoServiceImpl.java
@@ -0,0 +1,108 @@
+package com.apps.omnipotent.system.mongodb.service.impl;
+/*
+ *  Created by cles on 2020/12/1 23:10
+ */
+
+import com.apps.omnipotent.system.mongodb.service.MongoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.util.List;
+
+/**
+ * @author cles
+ * @description:
+ * @Date 2020/12/1 23:10
+ * @version: 1.0.0
+ */
+@Slf4j
+public  class MongoServiceImpl<T>  implements MongoService<T> {
+
+    @Autowired
+    public MongoTemplate mongoTemplate;
+
+    @Override
+    public void save(T o) {
+        log.info("[Mongo]save:" + o.toString());
+        mongoTemplate.save(o);
+    }
+
+    @Override
+    public T queryById(Class<T> cls, String id) {
+        Query query = new Query();
+        Criteria criteria = Criteria.where("_id").is(id);
+        query.addCriteria(criteria);
+        log.info(cls+"[Mongo]queryById:" + query);
+        return mongoTemplate.findOne(query, cls);
+    }
+
+    @Override
+    public List queryList(Class<T> cls, Query query) {
+        log.info(cls+"[Mongo]queryList:" + query);
+        return mongoTemplate.find(query, cls);
+
+    }
+
+    @Override
+    public T queryOne(Class<T> cls, Query query) {
+        log.info(cls+"[Mongo] queryOne:" + query);
+        return mongoTemplate.findOne(query, cls);
+    }
+
+    @Override
+    public List getPage(Class<T> cls, Query query, int start, int size) {
+        query.skip(start);
+        query.limit(size);
+        List<T> lists = mongoTemplate.find(query, cls);
+        log.info(cls+"[Mongo] queryPage:" + query + "(" + start +"," + size +")");
+        return lists;
+    }
+
+    @Override
+    public Long getPageCount(Class<T> cls, Query query) {
+        log.info(cls+"[Mongo]queryPageCount:" + query);
+        return mongoTemplate.count(query, cls);
+    }
+
+    @Override
+    public void deleteById(Class<T> cls, String id) {
+        Criteria criteria = Criteria.where("_id").in(id);
+        if(null!=criteria){
+            Query query = new Query(criteria);
+            if(null!=query && this.queryOne(cls,query)!=null){
+                mongoTemplate.remove(this.queryOne(cls,query));
+                log.info(cls+"[Mongo]deleteById:" + query);
+            }
+        }
+    }
+
+    @Override
+    public void delete(T t) {
+        mongoTemplate.remove(t);
+        log.info("[Mongo]delete:" + t);
+    }
+
+    @Override
+    public void updateFirst(Class<T> cls, String id, String objName, String objValue) {
+        Criteria criteria = Criteria.where("_id").in(id);
+        if(null!=criteria){
+            Query query = new Query(criteria);
+            if(null!= query && this.queryOne(cls,query)!=null){
+                Update update=new Update();
+                update.set(objName,objValue);
+                mongoTemplate.updateFirst(query,update,cls);
+                log.info(cls+"[Mongo]updateFirst:query(" + query + "),update(" + update + ")");
+            }
+        }
+    }
+
+    @Override
+    public void updateInser(Class<T> cls, Query query, Update update) {
+        log.info(cls+"[Mongo]updateInser:query(" + query + "),update(" + update + ")");
+        mongoTemplate.upsert(query, update, cls);
+    }
+}
